123e557d6e628b688ed0ce835376da15ad4cb17b
maletazul <luissilva@maletazul.pt>
2021-11-29 12:46:49 +0000

    fix ports

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
-var api = 'http://127.0.0.1:11898';
+var api = 'http://127.0.0.1:33898';
 var donationAddress = "";
 var blockTargetInterval = 30; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  100000000000000; // enter the total supply in atomic units
-var symbol = 'trtl'; // enter the coin's ticker
+var symbol = 'ime'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "trtl": [
+ "ime": [
 	["z-pool.com", "http://z-pool.com:8117"],
 	["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
 	["us.turtlepool.space", "http://us.turtlepool.space:8117"],
@@ -26,7 +26,7 @@ var networkStat = {
 };
 
 var networkStat2 = {
-    "trtl": [
+    "ime": [
 	[""]
  ]
 };
